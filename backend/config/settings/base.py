1924dd2fa6487f6766c8598b9621604d71544335
Rajiv Wallace <rajivghandi972@gmail.com>
2025-09-23 14:23:40 -0400

    initialize game, user and trivia apps

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # Local apps
+    'trivia',
 ]
 
 MIDDLEWARE = [
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls))
 ]
